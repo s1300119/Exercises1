9cf13b2ec2c592679744d0281d3d7b86cbd0aeb4
s1300119 <s130119@u-aizu.ac.jp>
2023-06-15 13:07:54 +0900

    Won/lost message modified

--- DiceGame.java
+++ DiceGame.java
@@ -3,27 +3,32 @@ import java.util.Scanner;
 
 public class DiceGame {
     public static void main(String[] args) {
+        // ユーザー名の入力を受け取る
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("What is your name? ");
+	System.out.print(">");
+        String name = scanner.nextLine();
 
-	Scanner scanner = new Scanner(System.in);
+        // ユーザーへの挨拶を表示
+        System.out.println("Hello, " + name + "!");
 
-	System.out.println("What is your name?");
-	String name = scanner.nextLine();
-	System.out.println("Hello, " + name + "!");
-
-	//System.out.println("Rolling dice...");
-        rollDice();
-    }
-
-    public static void rollDice() {
-        System.out.println("Rolling dice...");
+        // 2つのダイスを振る
         Random random = new Random();
         int die1 = random.nextInt(6) + 1;
         int die2 = random.nextInt(6) + 1;
 
+        // ダイスの結果を表示
+        System.out.println("Rolling the dice...");
         System.out.println("Die 1: " + die1);
         System.out.println("Die 2: " + die2);
-
         int totalValue = die1 + die2;
         System.out.println("Total value: " + totalValue);
+
+        // 勝敗の判定と結果の表示
+        if (totalValue > 7) {
+            System.out.println(name + " won!");
+        } else {
+            System.out.println(name + " lost.");
+        }
     }
 }
